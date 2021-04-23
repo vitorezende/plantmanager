32b51276e2b78360c0a443277185f48a3e184949
Vitor Rezende <vitorezende@gmail.com>
2021-04-22 22:19:43 -0400

    Tela de identificação do usuário

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Welcome } from './src/pages/Welcome';
+import { UserIdentification } from './src/pages/UserIdentification';
 import AppLoading from 'expo-app-loading'
 
 import {
@@ -18,6 +19,6 @@ export default function App (){
     return <AppLoading />
 
   return (
-      <Welcome />
+      <UserIdentification />
     )
 }
